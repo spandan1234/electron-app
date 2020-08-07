bc63704abb56af10f719001df42a6585a8ca7093
Spandan <spasamir@amazon.com>
2020-08-07 13:06:53 -0400

    working app with a title, image and slider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
+  const [zoomValue, setZoomValue] = useState("")
+
+    // const handleClick = () => {
+    //     console.log("button clicked")
+    //     // ipcRenderer.send(module.CATCH_ON_MAIN, 'ping')
+    // }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Everyday Millionaire</h1>
+      <img
+          src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1534285838l/41074642.jpg"
+          alt="Book Cover"/>
+
+      <h2>Current Zoom:</h2>
+
+      <br />
+      <input
+          type="range"
+          id="zoom"
+          min={1} max={5} step={1}
+          value={zoomValue} onChange={evt => setZoomValue(evt.target.value)}/>
+      <span id="sizeDetector"> </span>
+      {/*<button onClick={handleClick}/>*/}
+
+      <h3>{zoomValue}</h3>
     </div>
-  );
+    );
 }
 
 export default App;
